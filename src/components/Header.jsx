c160d3a53896b95fb85df2db26afd3917d81b4e8
amirxoder <amirxoder@gmail.com>
2022-08-23 12:58:56 +0430

    config login with firebase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,30 @@ import { MdShoppingBasket } from "react-icons/md";
 
 import Logo from "../img/logo.png";
 import Avatar from "../img/avatar.png";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app, firestore, storage } from "../firebase.config";
 
 const Header = () => {
+  const firebaseAuth = getAuth(app);
+
+  const Provider = new GoogleAuthProvider();
+
+  const login = async () => {
+    const res = await signInWithPopup(firebaseAuth, Provider);
+    console.log(res.user);
+  };
+
   return (
     <header className="w-screen fixed z-50  p-6 px-16">
       {/* desktop tablet */}
       <div className="hidden md:flex items-center justify-between h-full w-full  ">
-        <div className="flex items-center gap-2">
+        <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} alt="logo" className="w-8 object-cover" />
           <p className="text-headingColor text-xl font-bold">City</p>
-        </div>
+        </Link>
 
         <div className="flex items-center gap-8">
           <ul className="flex items-center gap-8 ">
@@ -53,11 +67,15 @@ const Header = () => {
             </div>
           </div>
 
-          <img
-            src={Avatar}
-            alt="userProfile"
-            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl"
-          />
+          <div className="relative">
+            <motion.img
+              whileTap={{ scale: 0.6 }}
+              src={Avatar}
+              onClick={login}
+              alt="userProfile"
+              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer"
+            />
+          </div>
         </div>
       </div>
 
